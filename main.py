b12d47842afb23ebad3622e5e8e0aedc93540607
Bola Ghaly <bola.ghaly@cix.csi.cuny.edu>
2022-02-12 18:45:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 import json
 #import random
 from app import keep_alive
-
+ 
 # variables
 discord_client = discord.Client()
 DB_Token = os.environ['DB-TOKEN']
@@ -16,7 +16,7 @@ def get_quote():
   quote = json_data[0]['q'] + " -" + json_data[0]['a']
   return(quote)
 
-#----- For Fun ---------------
+#------- For Fun
 def yes_no():
   response = requests.get("https://yesno.wtf/api/")
   json_data = json.loads(response.text)
@@ -27,7 +27,7 @@ def kanye_rest():
   json_data = json.loads(response.text)
   return json_data["quote"]
 
-#----- NASA Functions --------
+#-------- Educational/Helpful/Interesting
 def nasa_apod():
   response = requests.get("https://api.nasa.gov/planetary/apod?api_key=" + NASA_API_KEY)
   json_data = json.loads(response.text)
@@ -38,16 +38,65 @@ def bored():
   json_data = json.loads(response.text)
   return json_data["type"] , json_data["activity"]
 
-#apod()
-# def get_norm_joke():
-#   url = "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random"
-#   headers = {
-#       'accept': "application/json",
-#       'x-rapidapi-host': "matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
-#       'x-rapidapi-key': "53572b29b7mshbf323f61903a0a1p13b7a3jsna81ab2e2ca2b"
-#       }
-#   response = requests.request("GET", url, headers=headers)
-#   print(response.text)
+def fake_person_female():
+  response = requests.get("https://fakerapi.it/api/v1/persons?_quantity=1&_locale=en_US&_gender=female")
+  json_data = json.loads(response.text)
+  firstname = json_data["data"][0]['firstname']
+  lastname = json_data["data"][0]['lastname']
+  name = firstname + " " + lastname
+  gender = json_data["data"][0]['gender']
+  birthday = json_data["data"][0]['birthday']
+  phone = json_data["data"][0]['phone']
+  email = json_data["data"][0]['email']
+  address = json_data["data"][0]['address']["street"]
+  return (name, gender, birthday, phone, email, address)
+
+def fake_person_male():
+  response = requests.get("https://fakerapi.it/api/v1/persons?_quantity=1&_locale=en_US&_gender=male")
+  json_data = json.loads(response.text)
+  firstname = json_data["data"][0]['firstname']
+  lastname = json_data["data"][0]['lastname']
+  name = firstname + " " + lastname
+  gender = json_data["data"][0]['gender']
+  birthday = json_data["data"][0]['birthday']
+  phone = json_data["data"][0]['phone']
+  email = json_data["data"][0]['email']
+  address = json_data["data"][0]['address']["street"]
+  return (name, gender, birthday, phone, email, address)
+
+def fake_credit_Card():
+  response = requests.get("https://fakerapi.it/api/v1/credit_cards?_quantity=1")
+  json_data = json.loads(response.text)
+  type = json_data["data"][0]['type']
+  number = json_data["data"][0]['number']
+  expiration = json_data["data"][0]['expiration']
+  owner = json_data["data"][0]['owner']
+  return (type, owner, number, expiration)
+
+def next_marvel_movie():
+  response = requests.get("https://whenisthenextmcufilm.com/api")
+  json_data = json.loads(response.text)
+  title_1 = json_data['title']
+  days_until_1 = json_data['days_until']
+  release_date_1 = json_data['release_date']
+  type_1 = json_data['type']
+  overview_1 = json_data['overview']
+  poster_url_1 = json_data['poster_url']
+
+  return (title_1, days_until_1, release_date_1, type_1, overview_1, poster_url_1)
+
+def next_marvel_movie_afterwards():
+  response = requests.get("https://whenisthenextmcufilm.com/api")
+  json_data = json.loads(response.text)
+  title = json_data["following_production"]['title']
+  days_until = json_data["following_production"]['days_until']
+  release_date = json_data["following_production"]['release_date']
+  type = json_data["following_production"]['type']
+  overview = json_data["following_production"]['overview']
+  poster_url = json_data["following_production"]['poster_url']
+
+  return (title, days_until, release_date, type, overview, poster_url)
+
 
 @discord_client.event
 async def on_ready():
@@ -60,27 +109,55 @@ async def on_message(message):
 
   msg = message.content
   
-  if '$inspire' in msg:
+  if msg == '$inspire':
     quote = get_quote()
     await message.channel.send(quote)
 
+
   #------- For Fun
-  if '$yes/no' in msg:
+  if msg == '$yes/no':
     answer, image = yes_no()
     await message.channel.send("WALL-E Says: " + answer.capitalize()  + "\n" + image)
 
-  if '$kanye-rest' in msg:
+  if msg == '$kanye-rest':
     quote = kanye_rest()
     await message.channel.send(quote + " -Kanye West")
 
   #-------- Educational/Helpful/Interesting
-  if '$nasa-apod' in msg:
+  if msg == '$nasa-apod':
     title, url, date, explanation = nasa_apod()
     await message.channel.send(title + "\n" + url + "\n" + explanation)
 
-  if '$bored' in msg:
+  if msg == '$bored':
     type, activity = bored()
     await message.channel.send("```txt\nWALL-E suggests...\n\nActivity: " + activity + "\n" + "Type: " + type.capitalize() + "```")
+
+  if msg == '$fake-person-male':
+    name, gender, birthday, phone, email, address = fake_person_male()
+    await message.channel.send("```txt\nName: " + name + "\nGender: " + gender.capitalize() + "\nBirthday: " + birthday + "\nPhone: " + phone + "\nEmail: " + email + "\nAddress: " + address + "```")
+
+  if msg == '$fake-person-female':
+    name, gender, birthday, phone, email, address = fake_person_female()
+    await message.channel.send("```txt\nName: " + name + "\nGender: " + gender.capitalize() + "\nBirthday: " + birthday + "\nPhone: " + phone + "\nEmail: " + email + "\nAddress: " + address + "```")
+
+  if msg == '$fake-credit-card':
+    type, owner, number, expiration  = fake_credit_Card()
+    await message.channel.send("```txt\nCard Type: " + type + "\nCardholder Name: " + owner + "\nCard Number: " + number + "\nExpiration Date (mm/yy): " + expiration + "```")
+
+  if msg == '$upcoming-marvel':
+    title_1, days_until_1, release_date_1, type_1, overview_1, poster_url_1  = next_marvel_movie()
+    title_2, days_until_2, release_date_2, type_2, overview_2, poster_url_2  = next_marvel_movie_afterwards()
+    output_1 = (title_1 + " releases in " + str(days_until_1) + " days!\n\nRelease Date: " + release_date_1 + "\n\nProduction Type: " + type_1 + "\n\nOverview: " + overview_1 + "\n")
+    output_2 = (title_2 + " releases in " + str(days_until_2) + " days!\n\nRelease Date: " + release_date_2 + "\n\nProduction Type: " + type_2 + "\n\nOverview: " + overview_2 + "\n")
+    red_embed_1 = discord.Embed(color=0xff0000, title="When is the next Marvel Cinematic Universe (MCU) movie/tv show?", description=output_1)
+    red_embed_1.set_thumbnail(url=poster_url_1)
+    red_embed_1.set_footer(text="Source (Not my website): https://whenisthenextmcufilm.com/")
+    await message.channel.send(embed=red_embed_1)
+    red_embed_2 = discord.Embed(color=0xff0000, title="What's afterwards? you might ask...", description=output_2)
+    red_embed_2.set_thumbnail(url=poster_url_2)
+    red_embed_2.set_footer(text="Source (Not my website): https://whenisthenextmcufilm.com/")
+    await message.channel.send(embed=red_embed_2)
+
 
 # keeps on pinging the bot and keeps it running 24/7
 keep_alive()
